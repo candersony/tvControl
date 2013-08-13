092fbb0812e92d0053265267919cf0930085029e
Craig Anderson <craig@cander.me>
2013-08-14 00:51:06 +0100

    simple IR service to execute irsend commands

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
-var express = require("express");
- 
+var express = require("express"),
+    exec = require('child_process').exec;
+
 var app = express();
 app.use(express.logger());
 
 // Configuration
 
 app.configure(function(){
-  app.set('views', __dirname + '/app');
+    app.set('views', __dirname + '/app');
   //app.set('view engine', 'jade');
   app.use(express.bodyParser());
   app.use(express.methodOverride());
@@ -17,6 +18,17 @@ app.configure(function(){
 
 app.get('/', function(request, response) {
   response.render('index.html')
+});
+
+app.get('/ir/:device/:command', function(request, response){
+    var irCommand = {
+            device: request.params.device,
+        command: request.params.command
+    };
+
+    exec('irsend SEND_ONCE ' + irCommand.device + ' ' + irCommand.command.toUpperCase());
+
+    response.send(200, irCommand);
 });
 
 var port = process.env.PORT || 5000;
