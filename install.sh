79155d8e3a31a68638aaf42d59496d76d8ff291e
Craig Anderson <craig@cander.me>
2013-08-19 02:04:01 +0100

    started a full install script for the pi

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#sudo apt-get install lirc
+#sudo sed -i '$ a\lirc_dev' /etc/modules
+#sudo sed -i '$ a\lirc_rpi gpio_out_pin=22' /etc/modules
+
+wget http://nodejs.org/dist/v0.10.15/node-v0.10.15-linux-arm-pi.tar.gz
+tar -zxf node-v0.10.15-linux-arm-pi.tar.gz
